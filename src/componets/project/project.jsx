284c5e091d8ce0896f6609f689d0434659953280
minde8888 <mindebaltru@gmail.com>
2022-01-04 21:34:09 +0100

    table

--- src/componets/project/project.jsx
+++ src/componets/project/project.jsx
@@ -1,109 +1,151 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import AddProject from "./addProject";
 import DeleteProject from "./deleteProject";
 import uuid from "uuid";
+import { useState, useEffect } from "react";
 
-class Project extends Component {
-  constructor(props) {
-    super(props);
-    this.editModus = this.editModus.bind(this);
-    this.myRef = React.createRef();
+const Project = (props) => {
+  const [itemsList, setItemsList] = useState([]);
 
-    this.state = {
-      clicked: false,
-      id: null,
+  useEffect(() => {
+    getButtonList();
+  }, []);
+
+  const getButtonList = () => {
+    let data = [
+      {
+        id: uuid.v4(),
+        project: 35987,
+        name: "Mark",
+        address: "Oslo",
+        status: "in progress",
+        isSelected: false,
+      },
+      {
+        id: uuid.v4(),
+        project: 25987,
+        name: "Knack",
+        address: "Vilnius",
+        status: "in progress",
+        isSelected: false,
+      },
+      {
+        id: uuid.v4(),
+        project: 3,
+        name: "Shark",
+        address: "Moscow",
+        status: "finished",
+        isSelected: false,
+      },
+    ];
+
+    setItemsList(data);
+  };
+  const EditItemModus = ({ item }) => {
+
+    const btnClick = (event) => {
+      const id = event.target.attributes[1].value;
+     
+      setItemsList((e) =>
+        e.map((item) => ({
+          ...item,
+          isSelected: item.id === id,
+        }))
+      );
     };
-  }
 
-  createMarkup() {
-    return { __html: '<input type="text" value={1} onChange={saveEdit} />' };
-  }
+    const onChange = (e) => {
+      console.log(e);
+    };
 
-  editModus(e) {
-    console.log(e);
-    return <td dangerouslySetInnerHTML={this.createMarkup()} />;
-    // var elParent = document.getElementById(e.target.id);
-
-    // const newItem = document.createElement("td");
-    // newItem.innerHTML =
-    //   '<input type="text" value={1} onChange={onChange} />';
-    //  elParent.parentNode.replaceChild(newItem, elParent);
-  }
-
-  saveEdit(e){
-    console.log(e.target);
-  }
-  
-  render = () => {
-    const { clicked, id } = this.state;
     return (
-      <div>
-        <DeleteProject />
+      <tr>
+        <td>
+          <NavLink className="detail-icon" to="#"></NavLink>
+        </td>
+        <td className="bs-checkbox">
+          <label>
+            <input
+              data-index="0"
+              name="btSelectItem"
+              type="checkbox"
+              value="0"
+            />
+          </label>
+        </td>
+        <td
+          className={item.isSelected ? "d-none" : ""}
+          onClick={btnClick}
+          value={item.id}
+        >
+          {item.project}
+        </td>
+        <input type="text" value={1} onChange={onChange} />
 
-        <div className="bd-example">
-          <table className="table table-bordered">
-            <thead>
-              <tr>
-                <th className="detail">
-                  <div className="fht-cell"></div>
-                </th>
-                <th className="bs-checkbox " data-field="state">
-                  <div className="th-inner ">
-                    <label>
-                      <input name="btSelectAll" type="checkbox" />
-                    </label>
-                  </div>
-                  <div className="fht-cell"></div>
-                </th>
-                <th scope="col-2">Project nr</th>
-                <th scope="col">Name</th>
-                <th scope="col">Address</th>
-                <th scope="col">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <NavLink className="detail-icon" to="#"></NavLink>
-                </td>
-                <td className="bs-checkbox">
-                  <label>
-                    <input
-                      data-index="0"
-                      name="btSelectItem"
-                      type="checkbox"
-                      value="0"
-                    />
-                  </label>
-                </td>
-                <td id={uuid.v4()} onDoubleClick={this.editModus}>
-                  <span  scope="row">
-                    35987
-                  </span>
-                </td>
+        <td
+          className={item.isSelected ? "d-none" : ""}
+          onClick={btnClick}
+          value={item.id}
+        >
+          {item.name}
+        </td>
+        <td
+          className={item.isSelected ? "d-none" : ""}
+          onClick={btnClick}
+          value={item.id}
+        >
+          {item.address}
+        </td>
 
-                <td id={uuid.v4()} onDoubleClick={this.editModus}>
-                  <span >Mark</span>
-                </td>
-
-                <td id={uuid.v4()} onDoubleClick={this.editModus}>
-                  <span >Otto</span>
-                </td>
-
-                <td id={uuid.v4()} onDoubleClick={this.editModus}>
-                  <span >in progress</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <AddProject />
-      </div>
+        <td
+          className={item.isSelected ? "d-none" : ""}
+          onClick={btnClick}
+          value={item.id}
+        >
+          {item.status}
+        </td>
+      </tr>
     );
   };
-}
+
+  return (
+    <div>
+      <DeleteProject />
+
+      <div className="bd-example">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th className="detail">
+                <div className="fht-cell"></div>
+              </th>
+              <th className="bs-checkbox " data-field="state">
+                <div className="th-inner ">
+                  <label>
+                    <input name="btSelectAll" type="checkbox" />
+                  </label>
+                </div>
+                <div className="fht-cell"></div>
+              </th>
+              <th scope="col-2">Project nr</th>
+              <th scope="col">Name</th>
+              <th scope="col">Address</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemsList.map((item) => (
+              <EditItemModus key={item.id} item={item} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <AddProject />
+    </div>
+  );
+};
 
 function mapStateToProps(state) {
   const {} = state.user;
