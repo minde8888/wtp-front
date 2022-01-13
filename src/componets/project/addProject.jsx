16e31ad1247a8d74b181437e66bf838f3a7a671d
minde8888 <mindebaltru@gmail.com>
2022-01-14 00:00:16 +0100

    AddProject validation

--- src/componets/project/addProject.jsx
+++ src/componets/project/addProject.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
-import { connect } from "react-redux";
 import { addNewProject } from "../../redux/actions/projectData";
+import { connect } from "react-redux";
+import { setMessage } from "../../redux/actions/message";
+import plus from "../../svg/plus.svg";
 import "./addProject.scss";
 
 const AddProject = (props) => {
@@ -44,11 +46,26 @@ const AddProject = (props) => {
   };
 
   const handleClick = () => {
-    props.dispatch(addNewProject(allValues));
-    numberRef.current.value = null;
-    titleRef.current.value = "";
-    placeRef.current.value = "";
-    statusRef.current.value = "";
+    // if ( allValues.number === null) {
+    //   props.dispatch(setMessage("Project number can not by empty !"))
+    // }
+    allValues.number === null
+      ? props.dispatch(setMessage("Project number can not by empty !"))
+      : allValues.title === ""
+      ? props.dispatch(setMessage("Title can not by empty !"))
+      : allValues.place === ""
+      ? props.dispatch(setMessage("Place nr can not by empty !"))
+      : allValues.status === ""
+      ? props.dispatch(setMessage("Status nr can not by empty !"))
+      : props.dispatch(addNewProject(allValues));
+
+    // console.log(errors);
+    // console.log(a);
+    // props.dispatch(addNewProject(allValues));
+    // numberRef.current.value = null;
+    // titleRef.current.value = "";
+    // placeRef.current.value = "";
+    // statusRef.current.value = "";
   };
 
   const validateInputs = (e) => {
@@ -57,36 +74,11 @@ const AddProject = (props) => {
     else setErrors({ ...errors, [e.target.name]: false });
   };
 
-  const removeProjects = (e) => {
-    
-    console.log(props.removeProjects);
-    // array.filter((value, index, self) => self.indexOf(value) !== index))]
-    // console.log(uniq);
-  };
-
+  const {message} = props.message;
+console.log(message);
   return (
     <div className=" tb-actions">
       <div className="row ">
-        <div className="col-2 row justify-content-start">
-          <div className="d-button  col">
-            <button className=" dl" type="button" onClick={removeProjects}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                <path
-                  fillRule="evenodd"
-                  d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
         <div className="col-10 td-input row justify-content-end">
           <div className="col">
             <input
@@ -135,20 +127,27 @@ const AddProject = (props) => {
               type="button"
               onClick={() => handleClick()}
             >
-              &#43;
+              <img src={plus} alt="" />
             </button>
           </div>
         </div>
       </div>
+      {message && (
+        <div className="form-group">
+          <div className="alert alert-danger" role="alert">
+            {message}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 function mapStateToProps(state) {
 
-  const {removeProjects} = state.project;
+  const { message } = state;
 
-  return {removeProjects};
+  return { message };
 }
 
 export default connect(mapStateToProps)(AddProject);
--- src/componets/project/project.jsx
+++ src/componets/project/project.jsx
@@ -4,6 +4,7 @@ import EditItemModus from "./editItemModus";
 import { connect } from "react-redux";
 import { getAllProjects } from "../../redux/actions/projectData";
 import TablePreloader from "../preloader/tablePreloader";
+import trash from "../../svg/trash.svg";
 import "./project.scss";
 
 class Project extends Component {
@@ -11,42 +12,46 @@ class Project extends Component {
     this.props.dispatch(getAllProjects());
   }
 
+  removeProjects = (e) => {
+    
+    console.log(this.props.removeProjects);
+
+  };
+
   render = () => {
     return (
       <div>
         <AddProject />
-          <table className="table table-bordered">
-            <thead>
-              <tr>
-              <th className="bs-checkbox " data-field="state">
-                  <label>
-                    <input
-                      className="tb-input"
-                      name="btSelectAll"
-                      type="checkbox"
-                    />
-                  </label>
-                </th>
-                <th className="detail"></th>
-                <th scope="col-2">Project nr</th>
-                <th scope="col">Name</th>
-                <th scope="col">Address</th>
-                <th scope="col">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.projectIsLoaded ? <EditItemModus /> : <TablePreloader />}
-            </tbody>
-          </table>  
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th className="bs-checkbox" onClick={this.removeProjects}>
+                <img src={trash} alt="" />
+              </th>
+              <th className="detail"></th>
+              <th scope="col-2">Project nr</th>
+              <th scope="col">Name</th>
+              <th scope="col">Address</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.projectIsLoaded ? (
+              <EditItemModus />
+            ) : (
+              <TablePreloader />
+            )}
+          </tbody>
+        </table>
       </div>
     );
   };
 }
 
 function mapStateToProps(state) {
-  const { projectIsLoaded } = state.project;
+  const { projectIsLoaded, removeProjects } = state.project;
 
-  return { projectIsLoaded };
+  return { projectIsLoaded, removeProjects };
 }
 
 export default connect(mapStateToProps)(Project);
