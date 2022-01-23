814e8c91e413bae92fe8d350ae7ea034260c501d
minde8888 <mindebaltru@gmail.com>
2022-01-23 22:36:37 +0100

    calendar

--- src/helpers/emptyObject.js
+++ src/helpers/emptyObject.js
@@ -8,6 +8,16 @@ class EmptyObject {
    emptyValues = (obj) => {
       return Object.values(obj).every(x => (x === null || x === ''));
    }
+
+   removeEmptyObjectValues  = (obj) =>{
+      
+        Object.keys(obj).forEach((key) => {
+         if (obj[key] === "") {
+           delete obj[key];
+         }
+       });  
+       return obj;
+   }
 }
 
 export default new EmptyObject();
