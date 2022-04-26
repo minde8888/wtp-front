c722d0d5ef2e32436864b6c203a994582cc0f59d
minde8888 <mindebaltru@gmail.com>
2022-04-26 21:54:07 +0200

    react-select

--- src/components/progressPlan/contextMenu/addEmployees/addEmployees.jsx
+++ src/components/progressPlan/contextMenu/addEmployees/addEmployees.jsx
@@ -2,9 +2,9 @@ import React, { useRef, useState } from "react";
 import store from "../../../../redux/store";
 import { employeeAdd } from "../../../../redux/actions/progressPlan";
 import Select, { components } from "react-select";
-import style from "./addEmployees.module.scss"
+import style from "./addEmployees.module.scss";
 
-function MultiValueRemove({ employees }) {
+function AddEmployees({ employees }) {
   let options = employees.map((e) => {
     return { label: e.name + " " + e.surname, value: e.id };
   });
@@ -12,21 +12,29 @@ function MultiValueRemove({ employees }) {
   const employeeRef = useRef(null);
   store.dispatch(employeeAdd(employeeRef));
 
-  if (props.data.isFixed) {
-    return null;
-  }
-  return <components.MultiValueRemove {...options} />;
-};
+  // set value for default selection
+  const [selectedValue, setSelectedValue] = useState([]);
 
-export default AddEmployees = () => {
+  // handle onChange event of the dropdown
+  const handleChange = (e) => {
+    setSelectedValue(Array.isArray(e) ? e.map((x) => x.value) : []);
+  };
+  console.log(selectedValue);
   return (
-    <Select
-      isMulti
-      defaultValue={[colourOptions[0]]}
-      isClearable={false}
-      options={colourOptions}
-      components={{ MultiValueRemove }}
-    />
+    <div className={style.container}>
+      <Select
+        className="dropdown"
+        // placeholder="Select Option"
+        // isDisabled
+        value={options.filter((obj) => selectedValue.includes(obj.value))} // set selected values
+        options={options} // set list of the data
+        onChange={handleChange} // assign onChange function
+        // autoFocus 
+        isMulti
+        isClearable
+      />
+    </div>
   );
-};
+}
 
+export default AddEmployees;
